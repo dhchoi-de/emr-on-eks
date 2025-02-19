8a4485f0fc6e14a4a5e4ee8ac307b9cf400d8fc4
한국딥러닝 <koreadeep@hangugdibleoning-ui-noteubug.local>
2025-02-19 16:20:14 +0900

    upload tesseract ocr client

--- src/spark/spark_ocr.py
+++ src/spark/spark_ocr.py
@@ -13,13 +13,13 @@ aws_access_key = os.environ['ACCESS_KEY']
 aws_secret_access_key = os.environ['SECRET_ACCESS_KEY']
 bucket = os.environ['BUCKET']
 
-# spark = SparkSession.builder \
-#     .appName("PDF OCR with Spark on EMR on EKS") \
-#     .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.0,com.amazonaws:aws-java-sdk:1.11.1012") \
-#     .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
-#     .config("spark.hadoop.fs.s3a.access.key", f"{aws_access_key}") \
-#     .config("spark.hadoop.fs.s3a.secret.key", f"{aws_secret_access_key}") \
-#     .getOrCreate()
+spark = SparkSession.builder \
+    .appName("PDF OCR with Spark on EMR on EKS") \
+    .config("spark.jars.packages", "org.apache.hadoop:hadoop-aws:3.2.0,com.amazonaws:aws-java-sdk:1.11.1012") \
+    .config("spark.hadoop.fs.s3a.impl", "org.apache.hadoop.fs.s3a.S3AFileSystem") \
+    .config("spark.hadoop.fs.s3a.access.key", f"{aws_access_key}") \
+    .config("spark.hadoop.fs.s3a.secret.key", f"{aws_secret_access_key}") \
+    .getOrCreate()
 
 # PDF를 S3에서 읽어오는 함수
 def get_pdf_paths(s3_bucket_uri):
@@ -76,9 +76,10 @@ def run_ocr_spark(s3_bucket_uri):
     return result_df
 
 def run():
-
     s3_bucket_uri = f"s3a://{bucket}"
     result = run_ocr_spark(s3_bucket_uri)
     result.write.csv(f"s3a://{bucket}", header=True, mode="overwrite")
-    sp
-    # spark.stop()
+    spark.stop()
+
+if __name__ == "__main__":
+    run()
